942f829b058801fe27bf5c58b62a93999dec8a57
Julia Wisowaty <juliagw2@illinois.edu>
2018-04-26 17:49:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/juliawisowaty/finalprojectjuliajosh/Entertainment_Input.java
@@ -0,0 +1,13 @@
+package com.example.juliawisowaty.finalprojectjuliajosh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Entertainment_Input extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entertainment__input);
+    }
+}
---
+++ app/src/main/java/com/example/juliawisowaty/finalprojectjuliajosh/HomeScreen.java
@@ -0,0 +1,36 @@
+package com.example.juliawisowaty.finalprojectjuliajosh;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeScreen extends AppCompatActivity {
+    private Button b;
+    private Button a;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        b = (Button)findViewById(R.id.button);
+        a = (Button)findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeScreen.this,Food_Input.class);
+                startActivity(i);
+            }
+        });
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j = new Intent(HomeScreen.this, Entertainment_Input.class);
+                startActivity(j);
+
+            }
+        });
+    }
+}
