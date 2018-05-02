95cf8b3f4a5c1dba6fbae00ca0c740ee25f47832
Julia Wisowaty <juliagw2@illinois.edu>
2018-05-01 23:28:36 -0500

    11:27 PM

--- app/src/main/java/com/example/juliawisowaty/finalprojectjuliajosh/Food_Output.java
+++ app/src/main/java/com/example/juliawisowaty/finalprojectjuliajosh/Food_Output.java
@@ -2,12 +2,26 @@ package com.example.juliawisowaty.finalprojectjuliajosh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class Food_Output extends AppCompatActivity {
-
+public class Food_Output extends FragmentActivity{
+    protected GeoDataClient mGeoDataClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food__output);
+
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+        // TODO: Start using the Places API.
     }
 }
+
